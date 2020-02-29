a0264f9c96cc0bba09761672827026463b156811
Runmin Zhang <rzhang64@sheffield.ac.uk>
2020-03-01 05:06:09 +0800

    info get

--- app.py
+++ app.py
@@ -5,12 +5,30 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, Email
 from flask_mail import Mail, Message
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+
+@app.route('/testpage', methods=["POST", "GET"])
+def testpage():
+    destination = request.form.get('destination')
+    Airline_start = request.form.get('startplace')
+    Airline_end = request.form.get('endplace')
+    startday = request.form.get('startday')
+    endday = request.form.get('endday')
+    interest = request.form.getlist('interest')
+
+
+
+    return render_template("testPage.html", destination=destination,
+                           startplace=Airline_start, endplace=Airline_end,
+                           startday=startday, endday=endday,
+                           interest=interest)
 
 
 if __name__ == '__main__':
