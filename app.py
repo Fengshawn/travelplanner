fa083061b9f0208cc120e23ff0783ca284f8aa6b
Runmin Zhang <rzhang64@sheffield.ac.uk>
2020-03-11 06:17:08 +0800

    zrm

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
-from flask import Flask
-from flask import Flask, render_template, url_for, redirect, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length, Email
-from flask_mail import Mail, Message
-
+from flask import Flask, render_template, request,Session
+import sys
+import pdfkit
+config = pdfkit.configuration(wkhtmltopdf='D:\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
 app = Flask(__name__)
+sys.path.append("../../..")
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
+
+
+@app.route("/line", methods=["POST", "GET"])
+def line():
+    destination = request.form.get('destination')
+    Airline_start = request.form.get('startplace')
+    Airline_end = request.form.get('endplace')
+    startday = request.form.get('startday')
+    endday = request.form.get('endday')
+    interest = request.form.getlist('interest')
+    travellers = request.form.get('travellers')
+    Session['destination']=destination
+    Session['Airline_start']=Airline_start
+    Session['Airline_end']=Airline_end
+    Session['startday']=startday
+    Session['endday']=endday
+    Session['interest']=interest
+    Session['travellers']=travellers
+    return render_template("line.html", destination= Session['destination'],
+                           startplace=Airline_start, endplace=Airline_end, travellers=travellers,
+                           startday=startday, endday=endday,
+                           interest=interest)
 
 
 @app.route('/testpage', methods=["POST", "GET"])
@@ -22,13 +41,13 @@ def testpage():
     startday = request.form.get('startday')
     endday = request.form.get('endday')
     interest = request.form.getlist('interest')
-
-
+    travellers = request.form.get('travellers')
 
     return render_template("testPage.html", destination=destination,
-                           startplace=Airline_start, endplace=Airline_end,
+                           startplace=Airline_start, endplace=Airline_end, travellers=travellers,
                            startday=startday, endday=endday,
                            interest=interest)
+
 
 
 if __name__ == '__main__':
