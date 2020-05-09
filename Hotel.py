3c90b67fd7b63de28320c7a3a745453b1281dd03
Shuang Chen <schen142@sheffield.ac.uk>
2020-05-09 23:02:14 +0100

    Replace Hotel.py

--- Hotel.py
+++ Hotel.py
@@ -1,19 +1,13 @@
-"""
-@author:Areej
-@file:hotel.py
-
-"""
 from Models import hotel
 from amadeus import Client
 from opencage.geocoder import OpenCageGeocode
 
 
-def get_hotel(place):
-    """
+def hotel_postcode(args):
+    pass
 
-    :param place_code: suggests the best hotel deal based on the city selected
-    :return:
-    """
+
+def get_hotel(place):
     client = Client(
         client_id='YhUHykQeccgseWSSARlxtfIAzhdGgTM2',
         client_secret='IWBwclckwNgBKNWr'
@@ -23,21 +17,18 @@ def get_hotel(place):
     geocoder = OpenCageGeocode(key)  # Gecoding Service to get coordinates of city
     result = geocoder.geocode(place, no_annotations='1')  # Geo-coding Service API key
     # here we get only london (there will be many london in the world)
-    longitude = result[0]['geometry']['lng']  # Parsing Latitude
-    latitude = result[0]['geometry']['lat']  # Parsing Longitude
-    response = client.shopping.hotel_offers.get(latitude=latitude, longitude=longitude)
+    response = client.shopping.hotel_offers.get(get_hotel(place))
     print(response.data)
     # get all hotel number in london
     hotel_number = len(response.data)
     # use a list to store hotel information
     all_hotel = []
-    hotel_contact = "NULL"
-    hotel_address = "NULL"
-    hotel_name = "NULL"
-    hotel_rating = "4"
-    hotel_price = 95
-    hotel_latitude = 0
-    hotel_longitude = 0
+    hotel_contact = "21432321"
+    hotel_address = "A"
+    hotel_postcode = "S2 4QF"
+    hotel_name = "a"
+    hotel_rating = "7"
+    hotel_price = 150
     hotel_unit = 'GBP'
     # only choose top 3
     if (hotel_number <= 3):
@@ -50,17 +41,13 @@ def get_hotel(place):
                 hotel_address = temp_hotel['hotel']['address']['lines'][0]
             if ('contact' in temp_hotel['hotel'] and 'phone' in temp_hotel['hotel']['contact']):
                 hotel_contact = temp_hotel['hotel']['contact']['hotel']
-            if ('latitude' in temp_hotel['hotel']):
-                hotel_latitude = temp_hotel['hotel']['latitude']
-            if ('longitude' in temp_hotel['hotel']):
-                hotel_longitude = temp_hotel['hotel']['longitude']
             if ('price' in temp_hotel['offers'][0] and 'total' in temp_hotel['offers'][0]['price']):
                 hotel_price = temp_hotel['offers'][0]['price']['total']
-            if ('price' in temp_hotel['offers'][0] and 'currency' in temp_hotel['offers'][0]['price']):
-                hotel_unit = temp_hotel['offers'][0]['price']['currency']
+            if ('postcode' in temp_hotel['hotel']):
+                hotel_postcode = temp_hotel['hotel']['postcode']
+
             hotel_prase = hotel(name=hotel_name, unit=hotel_unit, rate=hotel_rating, price=hotel_price,
-                                latitude=hotel_latitude, longitude=hotel_longitude, communication=hotel_contact,
-                                position=hotel_address)
+                                 communication=hotel_contact, postcode= hotel_postcode, position=hotel_address)
             all_hotel.append(hotel_prase)
 
 
@@ -75,17 +62,11 @@ def get_hotel(place):
                 hotel_address = temp_hotel['hotel']['address']['lines'][0]
             if ('contact' in temp_hotel['hotel'] and 'phone' in temp_hotel['hotel']['contact']):
                 hotel_contact = temp_hotel['hotel']['contact']['phone']
-            if ('latitude' in temp_hotel['hotel']):
-                hotel_latitude = temp_hotel['hotel']['latitude']
-            if ('longitude' in temp_hotel['hotel']):
-                hotel_longitude = temp_hotel['hotel']['longitude']
             if ('price' in temp_hotel['offers'] and 'total' in temp_hotel['offers']['price']):
                 hotel_price = temp_hotel['offers']['price']['total']
-            if ('price' in temp_hotel['offers'][0] and 'currency' in temp_hotel['offers'][0]['price']):
-                hotel_unit = temp_hotel['offers'][0]['price']['currency']
+
             hotel_prase = hotel(name=hotel_name, unit=hotel_unit, rate=hotel_rating, price=hotel_price,
-                                latitude=hotel_latitude, longitude=hotel_longitude, communication=hotel_contact,
-                                position=hotel_address)
+                                communication=hotel_contact, postcode= hotel_postcode, position=hotel_address)
             all_hotel.append(hotel_prase)
     return all_hotel[0]
 
@@ -93,6 +74,3 @@ def get_hotel(place):
 if __name__ == '__main__':
     pass
     resp = get_hotel('Paris')
-    print(resp.latitude)
-    print(resp.longitude)
-    print("Done!")
