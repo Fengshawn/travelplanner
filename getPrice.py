1282a4362311875ee8384ccc6da7437736d7fc10
Areej <Aalosaimi2@sheffield.ac.uk>
2020-05-13 05:48:48 +0300

    Add getPrice.py: To get the sum of the total budget for all activities, hotel, transportation, and flight

---
+++ getPrice.py
@@ -0,0 +1,17 @@
+
+
+def get_price(hotel, flight, transportation):
+    """
+
+    :param hotel: Hotel Object
+    :param flight: Flight Object
+    :param transportation: List of transportation objects
+    :return:
+    """
+    total_price = 0
+    total_price += float(hotel.price)
+    total_price += float(flight['price'].split(' ')[0])
+    for transport in transportation:
+        if transport.total_price not in "N/A":
+            total_price += float(transport.total_price.split(' ')[0])
+    return total_price
