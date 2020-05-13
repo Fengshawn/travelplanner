119fad6a68a681417e32e2df75d0339655ce9e08
Areej <Aalosaimi2@sheffield.ac.uk>
2020-05-13 08:40:51 +0300

    Added photos to activities & resturant

--- Models.py
+++ Models.py
@@ -1,9 +1,8 @@
 """
-@author:Runmin Zhang&Yanghao Zhou
+@author:Runmin Zhang&Yanghao Zhou & Areej
 @file:Models.py
 @time:2020/03/28
 """
-
 
 class flight():
     def __init__(self, name, time, price, stop_times, package, details, cabin):
@@ -17,40 +16,50 @@ class flight():
 
 
 class hotel():
-    def __init__(self, name, price, position, communication, latitude, longitude, unit, rate):
+    def __init__(self, name, price, position, communication, latitude, longitude, unit, rate, days):
         self.name = name
-        self.price = price
+        self.price = str(float(price) * days)
         self.position = position
         self.communication = communication
         self.latitude = latitude
         self.longitude = longitude
         self.unit = unit
         self.rate = rate
+        self.type = 'hotel'
 
 
 class attraction():
-    def __init__(self, name, type, rating, latitude, longitude, address):
+    def __init__(self, name, type, rating, latitude, longitude, address, photo, time_range=None):
         self.name = name
-        self.type = type
+        self.type = type.title().replace("_", " ")
         self.rating = rating
         self.latitude = latitude
         self.longitude = longitude
         self.address = address
-        
+        self.duration = 3600*2
+        self.time_range = time_range
+        self.photo = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=500&photoreference={ photo }&key=AIzaSyDUu2EVWYs1E5Wv7xuaJGZTCBqeDMXeu4U"
 
 
 class restaurant():
-    def __init__(self, name, rating, latitude, longitude, address, type):
+    def __init__(self, name, rating, latitude, longitude, address, type, photo, time_range=None):
         self.name = name
         self.rating = rating
         self.latitude = latitude
         self.longitude = longitude
         self.address = address
-        self.type = type
+        self.type = type.title()
+        self.duration = 3600
+        self.time_range = time_range
+        self.photo = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=500&photoreference={ photo }&key=AIzaSyDUu2EVWYs1E5Wv7xuaJGZTCBqeDMXeu4U"
 
 
 class transportation():
-    def __init__(self, type, time, money):
-        self.type = type
-        self.time = time
-        self.money = money
+    def __init__(self, total_distance, total_duration, total_price, time_range, from_to):
+        self.type = 'transportation'
+        self.total_distance = total_distance
+        self.total_duration = total_duration
+        self.total_price = total_price
+        self.time_range = time_range
+        self.from_to = from_to
+
