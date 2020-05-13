b9d4667e2a05eaf1411c9a6f77c0427b44a2def0
Areej <Aalosaimi2@sheffield.ac.uk>
2020-05-13 23:29:54 +0300

    Fixed to add text & attach PDF file to send it to the user's email & add comments and organize the readability of the code.

--- EmailSend.py
+++ EmailSend.py
@@ -1,4 +1,5 @@
 """
+
 @author:ZRM&Areej
 @file:EmailSend.py
 @time:2020/03/29
@@ -10,32 +11,30 @@ from email.mime.text import MIMEText
 
 
 def send_email(toAddr):
-    fromAddr = 'travel.planner2030@gmail.com'
-    mypass = 'Sh12345678'
+    """
+    Send the PDF plan to the user's email address
+    :param toAddr: email address to send the plan to
+    :return:
+    """
+    fromAddr = 'travel.planner2030@gmail.com'   # Gmail Account
+    mypass = 'Sh12345678'  # Gmail Account Password
     msg = MIMEMultipart()
     msg['From'] = fromAddr
     msg['to'] = toAddr
     msg['Subject'] = 'Travel Planner'
     body = '''
-    #this is flight inforamtion
-
-    #this is hotel informaiton
-
-    #this is attractions infroamtion
-
+    The PDF for the travel plan is attached to this email
     '''
+    # Start Areej Part
     msg.attach(MIMEText(body, 'plain'))
     with open('static/temp/plan.pdf', "rb") as f:
-        # attach = email.mime.application.MIMEApplication(f.read(),_subtype="pdf")
-        attach = MIMEApplication(f.read(), _subtype="pdf")
-    attach.add_header('Content-Disposition', 'attachment', filename=str('static/temp/plan.pdf'))
+        attach = MIMEApplication(f.read(), _subtype="pdf")  # Attach the PDF to the email
+    attach.add_header('Content-Disposition', 'attachment', filename=str('plan.pdf'))
     msg.attach(attach)
+    # End Areej Part
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(fromAddr, mypass)
-    server.send_message(msg)
+    server.login(fromAddr, mypass)  # Login with account
+    server.send_message(msg)        # Send the email
+    server.quit()                   # Quit server once done
 
-    server.quit()
-
-if __name__ == "__main__":
-    send_email('travel.planner2030@gmail.com')
